d4c216b0e16fb1e1f31d3aa0aedfa3fe379c7dee
pooriaredorg <165016405+pooriaredorg@users.noreply.github.com>
2024-08-23 02:44:51 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 /**
 * @ts-nocheck   <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
-* Last Update: 4:20 UTC - Monday, 19 August 2024, By @Sahar-KM
+* Last Update: 4:20 UTC - Monday, 19 August 2024, By @pooriaredorg
 * Many thanks to github.com/bia-pain-bache
 */
 import { connect } from 'cloudflare:sockets';
